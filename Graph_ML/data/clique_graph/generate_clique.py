a032a1ecc0b29690ae0a76c0f76936db52311653
dmytro8765 <dima@bondaren.co>
2025-08-31 11:35:33 +0200

    Added restructured clique materials

---
+++ Graph_ML/data/clique_graph/generate_clique.py
@@ -0,0 +1,52 @@
+"""
+Generate a graph classification dataset.
+Labels:
+- 1: graph contains at least one k-clique;
+- 0: graph does contain any k-cliques.
+"""
+
+import random
+
+import networkx as nx
+import numpy as np
+import torch
+
+dataset = []
+num_samples = 3000
+type_graph = 0
+qubits = 6
+k = 3
+
+
+def contains_clique(graph):
+    return any(len(clique) == k for clique in nx.enumerate_all_cliques(graph))
+
+
+for _ in range(num_samples):
+    p = random.uniform(0.1, 0.9)
+
+    if type_graph == 0:
+        # Generate graph that CONTAINS at least one k-clique
+        g = nx.gnp_random_graph(qubits, p)
+        while not contains_clique(g):
+            p = random.uniform(0.15, 0.45)
+            g = nx.gnp_random_graph(qubits, p)
+        label = 1
+    else:
+        # Generate graph that DOES NOT contain any k-cliques
+        g = nx.gnp_random_graph(qubits, p)
+        while contains_clique(g):
+            p = random.uniform(0.15, 0.45)
+            g = nx.gnp_random_graph(qubits, p)
+        label = 0
+
+    array = np.append(nx.to_numpy_array(g).flatten(), label)
+    dataset.append(array)
+
+    type_graph = (type_graph + 1) % 2  # Alternate
+
+dataset = torch.tensor(np.array(dataset))
+torch.save(
+    dataset,
+    "/Users/home/qiskit_env/Pennylane/data/max_clique/graph_clique_nodes_6-graphs_3000.pt",
+)
